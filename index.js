45d4bb5e5e68a15c76d490b04bcb407200ba13f1
Tanish <tanishsingh@866gamil.com>
2025-10-23 22:50:35 +0530

    Added score board and start button

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("script working");
+document.addEventListener("DOMContentLoaded", ()=>{
+    const gameArena = document.getElementById("game-arena");
+    const arenaSize = 600;
+    const cellSize = 20;
+    let score = 0;
+    let gameStarted = false;
+    let food = {x:200, y:300};
+    let snake = [{x:120, y:200}, {x:140, y:200}, {x:160, y:200}];
+    let dx = cellSize;
+    let dy = 0;
+
+    function startGame(){
+        const scoreBoard = document.createElement("div");
+        scoreBoard.id = "Score-board";
+        document.body.insertBefore(scoreBoard, gameArena);
+
+        const startButton = document.createElement("button");
+        startButton.textContent = "Start Game";
+        startButton.classList.add("Start-button");
+        document.body.appendChild(startButton);
+    }
+    startGame();
+});
