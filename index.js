52921a60ec2356f85e7c219a71352df7d507e6a7
Tanish <tanishsingh@866gamil.com>
2025-10-24 11:58:09 +0530

    Added food item in the game Area

--- index.js
+++ index.js
@@ -9,15 +9,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let dx = cellSize;
     let dy = 0;
 
-    function startGame(){
+    function drawScoreBoard(){
+        const scoreBoard = document.getElementById("Score-board");
+        scoreBoard.textContent = `Score: ${score}`
+    }
+
+    function drawDiv(x, y, className){
+        const div = document.createElement("div");
+        div.classList.add(className);
+        div.style.top = `${y}px`;
+        div.style.left = `${x}px`;
+        return div;
+    }
+    function drawfoodAndSnake(){
+        gameArena.innerHTML = ''; // if previously something is drawn remove it 
+        // wipe out everything and redraw when snake is move
+        const foodElement = drawDiv(food.x, food.y, "food");
+        gameArena.appendChild(foodElement);
+    }
+    function gameLoop(){
+        setInterval(() => {
+            drawScoreBoard();
+            drawfoodAndSnake();
+        }, 1000);
+    }
+    function runGame(){
+        gameStarted = true;
+        gameLoop();
+    }
+    function initiateGame(){
         const scoreBoard = document.createElement("div");
         scoreBoard.id = "Score-board";
+        // scoreBoard.textContent = "10";
         document.body.insertBefore(scoreBoard, gameArena);
 
         const startButton = document.createElement("button");
         startButton.textContent = "Start Game";
         startButton.classList.add("Start-button");
         document.body.appendChild(startButton);
+
+        startButton.addEventListener('click', ()=>{
+            startButton.style.display = "none";
+            runGame();
+        })
     }
-    startGame();
+    initiateGame();
 });
